fe3e9207dea9c7fcc0ae3d2a1607d60d7307858f
Mweemba-3 <obviousmweemba15@gmail.com>
2026-08-10 01:56:09 +0200

    Updated workflow with migration fix

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -19,6 +19,7 @@ import 'package:record_web/record_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
 import 'package:video_player_web/video_player_web.dart';
+import 'package:workmanager_web/workmanager_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
@@ -36,5 +37,6 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   SharedPreferencesPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
   VideoPlayerPlugin.registerWith(registrar);
+  WorkmanagerWeb.registerWith(registrar);
   registrar.registerMessageHandler();
 }
